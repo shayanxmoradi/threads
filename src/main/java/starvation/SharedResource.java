6f4fcfa67aa2975c7b45d922614ef932c927b494
shayanxmoradi <shayan.moradi92@gmail.com>
2024-06-28 07:40:53 +0200

    starvation task completed. still needs some edits.

--- src/main/java/starvation/SharedResource.java
+++ src/main/java/starvation/SharedResource.java
@@ -10,7 +10,6 @@ class SharedResource {
         lock.lock();
         try {
             System.out.println(threadName + " acquired the lock.");
-            // Simulate some work with the resource
             Thread.sleep(1000);
             System.out.println(threadName + " released the lock.");
         } catch (InterruptedException e) {
